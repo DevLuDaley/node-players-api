597fc02fa966aab6b3648dc8697524e34a218f18
devludaley <daleyluis@gmail.com>
2021-10-04 00:17:08 -0400

    create seed file

---
+++ .history/src/seed_20211004001412.js
@@ -0,0 +1,9 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'nba_admin',
+  host: 'localhost',
+  database: 'sports_api',
+  password: 'password',
+  port: 5432,
+});
---
+++ src/seed.js
@@ -0,0 +1,10 @@
+const { Pool } = require('pg');
+const fetch = require('node-fetch');
+
+const pool = new Pool({
+  user: 'nba_admin',
+  host: 'localhost',
+  database: 'sports_api',
+  password: 'password',
+  port: 5432,
+});
